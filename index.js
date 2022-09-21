a71c9307fcec204c02049552a29e84ffb7c82cf3
samanthamarberger <slatham04@gmail.com>
2022-09-20 21:38:28 -0600

    commit tree 45bb232699e46c8800c12e83a1f54fd50ec15289

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(namesOfDrivers) {
+    let firstTwoDrivers = namesOfDrivers.slice(0, 2);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function(namesOfDrivers) {
+    let lastTwoDrivers = namesOfDrivers.slice(namesOfDrivers.length - 2, namesOfDrivers.length);
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function (fare){
+        return integer * fare;
+    }
+}
+
+const fareDoubler = function(fare) {
+    return fare * 2;
+}
+
+const fareTripler = function(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(namesOfDrivers, twoDrivers) {
+    return twoDrivers(namesOfDrivers);
+}
